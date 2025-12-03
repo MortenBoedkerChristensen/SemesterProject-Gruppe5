c2663bdcdf7a4637fa21ac498c5e66257d4d6e8d
Bastianhj1312 <10665995@ucn.dk>
2025-12-03 12:46:56 +0100

    Update GourmetBolcher.java

--- src/Model/GourmetBolcher.java
+++ src/Model/GourmetBolcher.java
@@ -1,12 +1,20 @@
 package Model;
 
 public class GourmetBolcher extends Candy {
-	public GourmetBolcher(int id, String type, int price, int stock, int minStock, int maxStock, java.sql.Date date) {
-		super(id, type, price, stock, minStock, maxStock, date);
-		
-		
-		
-		
-	}
 
+    public GourmetBolcher(int id, String type, int price, int stock, int minStock, int maxStock, java.sql.Date date) {
+        super(id, type, price, stock, minStock, maxStock, date);
+    }
+
+    public GourmetBolcher(Recipes recipe) {
+        super(
+            recipe.getProductID(),  // id
+            recipe.getName(),       // type / navn
+            recipe.getDifficulty(), // pris? hvis vi vil have difficulty med skal vi lave om på constructoren i Candy
+            0,                      // stock – vælg startlager
+            0,                      // minStock – definer evt.
+            100,                    // maxStock – eller anden værdi
+            new java.sql.Date(System.currentTimeMillis()) // dato
+        );
+    }
 }
