24c642f0a5c9df2f1280c8aba05d927377c6f859
Bastianhj1312 <10665995@ucn.dk>
2025-12-05 11:57:19 +0100

    candycontroller

--- src/Controller/CandyController.java
+++ src/Controller/CandyController.java
@@ -1,15 +1,74 @@
 package Controller;
 
-public class CandyController {
-	CandyDB cd = new CandyDB();
-	
-	public CandyController() {
-		
-	}
-	
-	
-	public List<Candy> getAllLowStockCandy() {
-		
-	}
+import java.util.ArrayList;
+import java.util.List;
 
+import Connection.DataAccessException;
+import Database.CandyDB;
+import Model.Bolcher;
+import Model.Candy;
+import Model.GourmetBolcher;
+import Model.LogoBolcher;
+import Model.Lollipop;
+
+public class CandyController {
+    private CandyDB candydb;
+    
+    public CandyController() throws DataAccessException {
+        candydb = new CandyDB();
+    }
+    
+    public Candy addCandy(
+            String type, 
+            int price, 
+            int stock, 
+            int minStock, 
+            int maxStock
+    ) throws DataAccessException {
+
+        java.sql.Date date = new java.sql.Date(System.currentTimeMillis());
+        Candy candy;
+
+        switch (type.toLowerCase()) {
+
+            case "bolcher":
+                candy = new Bolcher(0, type, price, stock, minStock, maxStock, date);
+                break;
+
+            case "logobolcher":
+                candy = new LogoBolcher(0, type, price, stock, minStock, maxStock, date);
+                break;
+
+            case "gourmetbolcher":
+                candy = new GourmetBolcher(0, type, price, stock, minStock, maxStock, date);
+                break;
+
+            case "lollipop":
+                candy = new Lollipop(0, type, price, stock, minStock, maxStock, date);
+                break;
+
+            default:
+                // fallback
+                candy = new Candy(0, type, price, minStock, maxStock, date, stock);
+                break;
+        }
+
+        return candydb.insert(candy);
+    }
+
+    public List<Candy> getAllLowStockCandy() throws DataAccessException {
+        List<Candy> lowStockList = new ArrayList<>();
+
+        // Henter alle candy fra databasen
+        List<Candy> allCandy = candydb.getAllCandy();
+
+        // Finder candy hvor stock < minStock
+        for (Candy c : allCandy) {
+            if (c.getStock() < c.getMinStock()) {
+                lowStockList.add(c);
+            }
+        }
+
+        return lowStockList;
+    }
 }
