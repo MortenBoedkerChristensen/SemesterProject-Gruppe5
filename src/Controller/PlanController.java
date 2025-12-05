2a1af02e1e8f871b478e76412feea0587f07c665
Felix M. <felix.benjamin.mathiassen@gmail.com>
2025-12-05 10:25:09 +0100

    Update PlanController.java

--- src/Controller/PlanController.java
+++ src/Controller/PlanController.java
@@ -1,6 +1,6 @@
 package Controller;
 
-import com.sun.tools.javac.util.List;
+
 
 import Model.Plan;
 import Model.Recipes;
@@ -24,10 +24,7 @@ public class PlanController {
 		//planDB save plan
 	}
 	
-	public List<Recipes> getLowStockRecipes() {
-		List<Recipes> recipes = new List<>();
-		return recipes;
-	}
+	
 	
 	
 	
