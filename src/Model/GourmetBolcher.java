27be0945f6b0c7ceea099adae71a76c675f5ce54
Bastianhj1312 <10665995@ucn.dk>
2025-12-08 11:30:52 +0100

    tilføjet name til candy klasserne

--- src/Model/GourmetBolcher.java
+++ src/Model/GourmetBolcher.java
@@ -1,19 +1,23 @@
 package Model;
 
+import java.sql.Date;
+
 public class GourmetBolcher extends Candy {
-	public GourmetBolcher(int id, String type, int price, int stock, int minStock, int maxStock, java.sql.Date date) {
-		super(id, type, price, stock, minStock, date, maxStock);
-		
-	}
-	public GourmetBolcher(Candy candy, Recipes recipe) {
-	    super(
-	        recipe.getCandyID(),      // id
-	        recipe.getName(),           // type
-	        candy.getPrice(),           // price
-	        candy.getStock(),           // stock — brug evt. candy's stock
-	        candy.getMinStock(),        // minStock
-	        new java.sql.Date(System.currentTimeMillis()),        // maxStock
-	        candy.getMaxStock()
-	    );
-	}
+
+    public GourmetBolcher(int id, String type, int price, int stock, int minStock, int maxStock, Date date, String name) {
+        super(id, type, price, minStock, maxStock, date, stock, name);
+    }
+
+    public GourmetBolcher(Candy candy, Recipes recipe) {
+        super(
+            recipe.getCandyID(),
+            candy.getType(),
+            candy.getPrice(),
+            candy.getMinStock(),
+            candy.getMaxStock(),
+            new Date(System.currentTimeMillis()),
+            candy.getStock(),
+            recipe.getName()
+        );
+    }
 }
